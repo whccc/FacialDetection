89f556ab35c8ef690c82a54e6068a0a44706f665
Wilson Castro <wilsoncastro0710@gmail.com>
2021-02-06 12:35:46 -0500

    clonc

---
+++ clone-zoom-main/clone-zoom-main/server/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const server = require("http").Server(app);
+app.use(cors());
+const io = require("socket.io")(server);
+const port = 3000;
+
+io.on("connection", (socket) => {
+  socket.on("join", (data) => {
+    console.log(data);
+    const roomName = data.roomName;
+    socket.join(roomName);
+    socket.to(roomName).broadcast.emit("new-user", data);
+
+    socket.on("disconnect", () => {
+      socket.to(roomName).broadcast.emit("bye-user", data);
+    });
+  });
+});
+
+server.listen(process.env.PORT || port, () => {
+  console.log(`Server running port ${port}`);
+});
